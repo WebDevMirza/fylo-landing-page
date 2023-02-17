0d16741d6de9a9886e9b8e8150668008d83c4fc8
WebDev Mirza <webdevmirza@gmail.com>
2023-02-17 22:02:10 +0600

    hero completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import Styles from "../styles/Hero.module.css";
+import HeroImg from "../assets/images/illustration-1.svg";
+import Image from "next/image";
+import Forms from "@/utility/Forms";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="layout">
+        <div className={Styles["hero"]}>
+          <div className={Styles["hero-img"]}>
+            <Image src={HeroImg} alt="hero img" />
+          </div>
+          <div className={Styles["hero-body"]}>
+            <h1 className="h1">All your files in one secure location, accessible anywhere.</h1>
+            <p>
+              Fylo stores your most important files in one secure location. Access them wherever you need, share and
+              collaborate with friends, family, and co-workers.
+            </p>
+            <Forms />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
